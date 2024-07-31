63272d9484a2e97ab3069b337a3fd978be76f7e8
saurabh6789 <127424178+saurabh6789@users.noreply.github.com>
2024-08-01 00:57:51 +0530

    Update JobDetails.jsx

--- frontend/src/components/Job/JobDetails.jsx
+++ frontend/src/components/Job/JobDetails.jsx
@@ -12,7 +12,7 @@ const JobDetails = () => {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:4000/api/v1/job/${id}`, {
+      .get(`https://mern-stack-job-seeking-web-application-92cq.onrender.com/api/v1/job/${id}`, {
         withCredentials: true,
       })
       .then((res) => {
