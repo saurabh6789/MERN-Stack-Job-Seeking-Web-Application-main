08456082ea2116138ce2b23c2e71360b1f217a1c
saurabh6789 <127424178+saurabh6789@users.noreply.github.com>
2024-08-01 01:19:05 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(
     // method: ["GET", "POST", "DELETE", "PUT"],
     // credentials: true,
     // // Access-Control-Allow-Origin: *
-    origin: 'http://localhost:5173',
+    origin: 'https://mern-stack-job-seeking-web-application-92cq.onrender.com',
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials:true
