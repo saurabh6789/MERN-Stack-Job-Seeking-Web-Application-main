78ea7450eb937ffe2a5cffdd34b9c6d86d78570b
saurabh6789 <127424178+saurabh6789@users.noreply.github.com>
2024-08-01 00:59:45 +0530

    Update Navbar.jsx

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:4000/api/v1/user/logout",
+        "https://mern-stack-job-seeking-web-application-92cq.onrender.com/api/v1/user/logout",
         {
           withCredentials: true,
         }
