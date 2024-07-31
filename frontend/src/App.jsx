7d3e75f10467a92ce5e53f08ebdecfa56169ac04
saurabh6789 <127424178+saurabh6789@users.noreply.github.com>
2024-08-01 00:53:57 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/getuser",
+          "https://mern-stack-job-seeking-web-application-92cq.onrender.com/api/v1/user/getuser",
           {
             withCredentials: true,
           }
