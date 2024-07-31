099a233388a2da8320c5d79fe91236278053353c
saurabh6789 <127424178+saurabh6789@users.noreply.github.com>
2024-08-01 00:55:38 +0530

    Update Login.jsx

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/user/login",
+        "https://mern-stack-job-seeking-web-application-92cq.onrender.com/api/v1/user/login",
         { email, password, role },
         {
           headers: {
