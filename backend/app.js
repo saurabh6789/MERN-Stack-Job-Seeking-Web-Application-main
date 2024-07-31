5d4ccc2c8f53133ed2223c0d85b34b2cb81cde3f
saurabh6789 <127424178+saurabh6789@users.noreply.github.com>
2024-08-01 01:47:05 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(
     // method: ["GET", "POST", "DELETE", "PUT"],
     // credentials: true,
     // // Access-Control-Allow-Origin: *
-    origin: 'https://jobfinderfronted-qizmw2uth-saurabh6789s-projects.vercel.app/',
+    origin: 'https://jobfinderfronted-qizmw2uth-saurabh6789s-projects.vercel.app',
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials:true
